194950edc2888b09af2edcac0c22a73ddd62ddb6
+ <+>
2024-05-13 06:15:28 +0300

    cope

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,40 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
 
 function Movie() {
+  const [movie, setMovie] = useState(null);
+  const params = useParams();
+  const movieId = params.id;
+  
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${movieId}`)
+    .then(res => res.json())
+    .then(data => setMovie(data))
+    .catch(error => console.error(error))
+  }, [movieId])
+
+  if(!movie) {
+    return <h1>Loading...</h1>
+  }
+
+
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time}</p>
+        <div>
+          {movie.genres.map((genre, index) => (
+            <span key={index}>{genre}</span>
+          ))}
+        </div>
       </main>
     </>
   );
